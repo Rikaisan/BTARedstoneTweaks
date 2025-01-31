2bf94b3801963ebcf12281228b97e65ccbbbbda5
Rikai <rikaisensei@gmail.com>
2025-01-31 13:45:04 -0500

    refactor(Mixin): Remove overwrite in favor of a redirect

--- src/main/java/com/rikaisan/mixin/WorldMixin.java
+++ src/main/java/com/rikaisan/mixin/WorldMixin.java
@@ -6,29 +6,23 @@ import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import net.minecraft.core.world.WorldSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = World.class, remap = false)
 public abstract class WorldMixin {
 
-	/**
-	 * @author Rikai
-	 * @reason Make redstone pumpkin behave like a solid block
-	 */
-	@Overwrite
-	public boolean getSignal(int x, int y, int z, Side side) {
-		Block<?> block = this.getBlock(x, y, z);
-		if (this.isBlockNormalCube(x, y, z) && block != Blocks.BLOCK_REDSTONE && block != Blocks.PUMPKIN_REDSTONE) {
-			return this.hasDirectSignal(x, y, z);
-		} else if (block == Blocks.PUMPKIN_REDSTONE) { // Patch to make the redstone pumpkin behave like a solid block
-			return pumpkinHasDirectSignal(x, y, z) || block.getSignal((WorldSource) this, x, y, z, side);
-		}
-		else {
-			return block != null && block.getSignal((WorldSource) this, x, y, z, side);
+	@Redirect(method = "getSignal(IIILnet/minecraft/core/util/helper/Side;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/block/Block;getSignal(Lnet/minecraft/core/world/WorldSource;IIILnet/minecraft/core/util/helper/Side;)Z"))
+	public boolean getSignal(Block<?> instance, WorldSource worldSource, int x, int y, int z, Side side) {
+		if (instance == Blocks.PUMPKIN_REDSTONE) {
+			return pumpkinHasDirectSignal(x, y, z) || instance.getSignal(worldSource, x, y, z, side);
+		} else {
+			return instance.getSignal(worldSource, x, y, z, side);
 		}
 	}
+
 	// Special version of hasDirectSignal that excludes the front side of the redstone lantern.
 	@Unique
 	private boolean pumpkinHasDirectSignal(int x, int y, int z) {
