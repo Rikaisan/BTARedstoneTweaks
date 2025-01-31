46a47ba0fef092099d5854bfc03a6b9b889a5a9a
Rikai <rikaisensei@gmail.com>
2025-01-31 15:47:37 -0500

    refactor(BlockLogicWireRedstoneMixin): Replace overwrite with inject

--- src/main/java/com/rikaisan/mixin/BlockLogicWireRedstoneMixin.java
+++ src/main/java/com/rikaisan/mixin/BlockLogicWireRedstoneMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.core.util.helper.Axis;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.WorldSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,17 +22,13 @@ public abstract class BlockLogicWireRedstoneMixin {
 	@Shadow
 	private boolean shouldSignal;
 
-	// TODO: See if this can become an injection instead of an overwrite.
-	/**
-	 * @author Rikai
-	 * @reason Refactor + Fix redirection
-	 */
-	@Overwrite
-	public boolean getSignal(WorldSource worldSource, int x, int y, int z, Side side) {
+	// TODO: Improve compatibility by trying to break down this into smaller injections.
+	@Inject(method = "getSignal(Lnet/minecraft/core/world/WorldSource;IIILnet/minecraft/core/util/helper/Side;)Z", at = @At("HEAD"), cancellable = true)
+	public void getSignal(WorldSource worldSource, int x, int y, int z, Side side, CallbackInfoReturnable<Boolean> cir) {
 		if (!this.shouldSignal || worldSource.getBlockMetadata(x, y, z) == 0) {
-			return false;
+			cir.setReturnValue(false);
 		} else if (side == Side.TOP) {
-			return true;
+			cir.setReturnValue(true);
 		} else {
 			boolean negXShouldConnectTo = shouldConnectTo(worldSource, x - 1, y, z, 1)
 				|| !worldSource.isBlockNormalCube(x - 1, y, z) && AdditionalRedstoneWireLogic.shouldConnectToDiagonal(worldSource, x - 1, y - 1, z, -1, Side.TOP);
@@ -62,23 +57,26 @@ public abstract class BlockLogicWireRedstoneMixin {
 				}
 			}
 
-		boolean isXConnected = posXShouldConnectTo || negXShouldConnectTo;
-		boolean isZConnected = posZShouldConnectTo || negZShouldConnectTo;
+			boolean isXConnected = posXShouldConnectTo || negXShouldConnectTo;
+			boolean isZConnected = posZShouldConnectTo || negZShouldConnectTo;
 
-		// Refactor + Patch to make redirection work as intended
-		return !isZConnected && !isXConnected && side.getAxis() != Axis.Y // Default single dust
-			// When the signal request comes from a block that dust connects to (usually power sources)
-			|| side == Side.SOUTH && negZShouldConnectTo
-			|| side == Side.NORTH && posZShouldConnectTo
-			|| side == Side.EAST && negXShouldConnectTo
-			|| side == Side.WEST && posXShouldConnectTo
-			// When the signal request comes from the block opposite of a connection (most redstone components)
-			|| side == Side.NORTH && negZShouldConnectTo && !isXConnected
-			|| side == Side.SOUTH && posZShouldConnectTo && !isXConnected
-			|| side == Side.WEST && negXShouldConnectTo && !isZConnected
-			|| side == Side.EAST && posXShouldConnectTo && !isZConnected;
+			// Refactor + Patch to make redirection work as intended
+			cir.setReturnValue(
+				!isZConnected && !isXConnected && side.getAxis() != Axis.Y // Default single dust
+				// When the signal request comes from a block that dust connects to (usually power sources)
+				|| side == Side.SOUTH && negZShouldConnectTo
+				|| side == Side.NORTH && posZShouldConnectTo
+				|| side == Side.EAST && negXShouldConnectTo
+				|| side == Side.WEST && posXShouldConnectTo
+				// When the signal request comes from the block opposite of a connection (most redstone components)
+				|| side == Side.NORTH && negZShouldConnectTo && !isXConnected
+				|| side == Side.SOUTH && posZShouldConnectTo && !isXConnected
+				|| side == Side.WEST && negXShouldConnectTo && !isZConnected
+				|| side == Side.EAST && posXShouldConnectTo && !isZConnected
+			);
 		}
 	}
+
 	// Connect to both the front and back of the repeater, instead of only the back.
 	@Inject(method = "shouldConnectTo", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/WorldSource;getBlockMetadata(III)I", ordinal = 1), cancellable = true)
 	private static void connectToFrontBackRepeater(WorldSource worldSource, int x, int y, int z, int data, CallbackInfoReturnable<Boolean> cir) {
