4af61cb4ae80c5b4a6e8a33e1900f054f3044548
CelDaemon <celdaemon@voidgroup.net>
2025-01-31 14:01:12 +0100

    Prevent redstone lantern feeding back into itself and activating other sides

--- src/main/java/com/rikaisan/mixin/WorldMixin.java
+++ src/main/java/com/rikaisan/mixin/WorldMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.core.world.WorldSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = World.class, remap = false)
 public abstract class WorldMixin {
@@ -22,11 +23,23 @@ public abstract class WorldMixin {
 		if (this.isBlockNormalCube(x, y, z) && block != Blocks.BLOCK_REDSTONE && block != Blocks.PUMPKIN_REDSTONE) {
 			return this.hasDirectSignal(x, y, z);
 		} else if (block == Blocks.PUMPKIN_REDSTONE) { // Patch to make the redstone pumpkin behave like a solid block
-			return this.hasDirectSignal(x, y, z) || block.getSignal((WorldSource) this, x, y, z, side);
+			return pumpkinHasDirectSignal(x, y, z) || block.getSignal((WorldSource) this, x, y, z, side);
 		}
 		else {
 			return block != null && block.getSignal((WorldSource) this, x, y, z, side);
 		}
+	}
+	// Special version of hasDirectSignal that excludes the front side of the redstone lantern.
+	@Unique
+	private boolean pumpkinHasDirectSignal(int x, int y, int z) {
+		for (Side side : Side.sides) {
+			if(Side.getSideById(getBlockMetadata(x, y, z)) == side) continue;
+			if (getDirectSignal(x + side.getOffsetX(), y + side.getOffsetY(), z + side.getOffsetZ(), side)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	@Shadow
@@ -37,5 +50,11 @@ public abstract class WorldMixin {
 
 	@Shadow
 	public abstract Block<?> getBlock(int x, int y, int z);
+
+	@Shadow
+	public abstract boolean getDirectSignal(int x, int y, int z, Side side);
+
+	@Shadow
+	public abstract int getBlockMetadata(int x, int y, int z);
 }
 
