9313a29170b7eecc6df677f6bf3063226fc2c036
Rikai <rikaisensei@gmail.com>
2025-01-31 15:18:47 -0500

    feat(RedstoneBlock): Replace overwrite with inject

--- src/main/java/com/rikaisan/mixin/BlockLogicRedstoneMixin.java
+++ src/main/java/com/rikaisan/mixin/BlockLogicRedstoneMixin.java
@@ -5,17 +5,15 @@ import net.minecraft.core.block.BlockLogicRedstone;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = BlockLogicRedstone.class, remap = false)
 public abstract class BlockLogicRedstoneMixin {
 
-	/**
-	 * @author Rikai
-	 * @reason Make redstone blocks not hard power surrounding blocks
-	 */
-	@Overwrite
-	public boolean getDirectSignal(World world, int x, int y, int z, Side side) {
-			return world.getGameRuleValue(RedstoneTweaks.REDSTONE_BLOCK_HARD_POWER);
+	@Inject(method = "getDirectSignal(Lnet/minecraft/core/world/World;IIILnet/minecraft/core/util/helper/Side;)Z", at = @At("RETURN"), cancellable = true)
+	public void getDirectSignal(World world, int x, int y, int z, Side side, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(world.getGameRuleValue(RedstoneTweaks.REDSTONE_BLOCK_HARD_POWER));
 	}
 }
