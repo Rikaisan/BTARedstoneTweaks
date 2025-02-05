80a3db19e88c94daf242e6bcbe5b49e38ef4bf8f
CelDaemon <celdaemon@voidgroup.net>
2025-02-05 18:32:29 +0100

    Use player's direction as the lever rotation directly instead of the perpendicular direction.

---
+++ src/main/java/com/rikaisan/mixin/BlockLogicLeverMixin.java
@@ -0,0 +1,15 @@
+package com.rikaisan.mixin;
+
+import net.minecraft.core.block.BlockLogicLever;
+import net.minecraft.core.util.helper.Axis;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(value = BlockLogicLever.class, remap = false)
+public class BlockLogicLeverMixin {
+
+	@Redirect(method = "onBlockPlacedByMob", at = @At(value = "FIELD", target = "Lnet/minecraft/core/util/helper/Axis;Z:Lnet/minecraft/core/util/helper/Axis;"))
+	private Axis a() {
+		return Axis.X;
+	}
+}
