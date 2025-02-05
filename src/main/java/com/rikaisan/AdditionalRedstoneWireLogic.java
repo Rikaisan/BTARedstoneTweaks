f44ad4e89ef6be3cffc2687fbc15a5d68cb3fd7e
Rikai <rikaisensei@gmail.com>
2025-02-05 11:22:26 -0500

    refactor(AdditionalRedstoneWireLogic): Replace block ID check with block instance check for future proofing

--- src/main/java/com/rikaisan/AdditionalRedstoneWireLogic.java
+++ src/main/java/com/rikaisan/AdditionalRedstoneWireLogic.java
@@ -1,5 +1,6 @@
 package com.rikaisan;
 
+import net.minecraft.core.block.Block;
 import net.minecraft.core.block.Blocks;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.WorldSource;
@@ -8,7 +9,6 @@ public class AdditionalRedstoneWireLogic {
 	public static boolean shouldConnectToDiagonal(WorldSource worldSource, int x, int y, int z, int data, Side side) {
 		if (!(side == Side.TOP || side == Side.BOTTOM)) return false; // Maybe some can generate a connection if placed below a block in the future?
 
-		int blockId = worldSource.getBlockId(x, y, z);
-		return blockId == Blocks.WIRE_REDSTONE.id();
+		return worldSource.getBlock(x, y, z) == Blocks.WIRE_REDSTONE;
 	}
 }
