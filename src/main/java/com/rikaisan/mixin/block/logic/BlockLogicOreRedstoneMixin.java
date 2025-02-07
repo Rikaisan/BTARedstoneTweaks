dc6bed55dbc746ad1f4daaeb17b6c630d9326752
Rikai <rikaisensei@gmail.com>
2025-02-07 12:22:15 -0500

    feat(RedstoneOre): Remove no longer needed overrides to send updates

--- src/main/java/com/rikaisan/mixin/block/logic/BlockLogicOreRedstoneMixin.java
+++ src/main/java/com/rikaisan/mixin/block/logic/BlockLogicOreRedstoneMixin.java
@@ -4,8 +4,6 @@ import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
 import net.minecraft.core.block.BlockLogicOreRedstone;
 import net.minecraft.core.block.material.Material;
-import net.minecraft.core.util.helper.Side;
-import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BlockLogicOreRedstone.class)
@@ -13,20 +11,6 @@ public abstract class BlockLogicOreRedstoneMixin extends BlockLogic {
 
 	public BlockLogicOreRedstoneMixin(Block<?> block, Material material) {
 		super(block, material);
-	}
-
-	@Override
-	public void onBlockPlacedByWorld(World world, int x, int y, int z) {
-		for (Side s : Side.sides) {
-			world.notifyBlocksOfNeighborChange(x + s.getOffsetX(), y + s.getOffsetY(), z + s.getOffsetZ(), this.id());
-		}
-	}
-
-	@Override
-	public void onBlockRemoved(World world, int x, int y, int z, int data) {
-		for (Side s : Side.sides) {
-			world.notifyBlocksOfNeighborChange(x + s.getOffsetX(), y + s.getOffsetY(), z + s.getOffsetZ(), this.id());
-		}
 	}
 
 	@Override
