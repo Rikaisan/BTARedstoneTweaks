69ced8e1afd22fe59ae2cd389ccb5567b3e0f2da
Rikai <rikaisensei@gmail.com>
2025-07-13 04:13:51 -0500

    feat(Mesh): Both mesh blocks can now be blocked by powering them with redstone

---
+++ src/main/java/com/rikaisan/mixin/block/logic/BlockLogicMeshGoldMixin.java
@@ -0,0 +1,18 @@
+package com.rikaisan.mixin.block.logic;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.block.BlockLogicMeshGold;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = BlockLogicMeshGold.class, remap = false)
+public class BlockLogicMeshGoldMixin {
+	@ModifyExpressionValue(method = "collidesWithEntity(Lnet/minecraft/core/entity/Entity;Lnet/minecraft/core/world/World;III)Z", at = @At(value = "FIELD", target = "Lnet/minecraft/core/block/entity/TileEntityMeshGold;filterItem:Lnet/minecraft/core/item/ItemStack;"))
+	private ItemStack checkForPower(ItemStack original, @Local(name = "world") World world, @Local(name = "x") int x, @Local(name = "y") int y, @Local(name = "z") int z) {
+		if (world.hasNeighborSignal(x, y, z)) return null;
+		return original;
+	}
+}
---
+++ src/main/java/com/rikaisan/mixin/block/logic/BlockLogicMeshMixin.java
@@ -0,0 +1,17 @@
+package com.rikaisan.mixin.block.logic;
+
+import net.minecraft.core.block.BlockLogicMesh;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = BlockLogicMesh.class, remap = false)
+public class BlockLogicMeshMixin {
+	@Inject(method = "collidesWithEntity(Lnet/minecraft/core/entity/Entity;Lnet/minecraft/core/world/World;III)Z", at = @At("TAIL"), cancellable = true)
+	private void checkForPower(Entity entity, World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+		if (world.hasNeighborSignal(x, y, z)) cir.setReturnValue(true);
+	}
+}
